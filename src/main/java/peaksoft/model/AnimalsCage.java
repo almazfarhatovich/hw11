a23f8050d83cf13345443e3b2ee166825411f9f3
almazfarhatovich <janybekovalmaz29@gmail.com>
2022-05-14 20:10:00 +0600

    finished

--- src/main/java/peaksoft/model/AnimalsCage.java
+++ src/main/java/peaksoft/model/AnimalsCage.java
@@ -3,8 +3,6 @@ package peaksoft.model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
